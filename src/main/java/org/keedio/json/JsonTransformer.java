62e4338063e04413af6af4b4d605ccacc8d84c0e
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-06-11 15:23:37 +0200

    Make transformation agnostic of the type of objects in the array to expand and return a list of JSON strings.

--- src/main/java/org/keedio/json/JsonTransformer.java
+++ src/main/java/org/keedio/json/JsonTransformer.java
@@ -3,9 +3,12 @@ package org.keedio.json;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class JsonTransformer {
 
-    public static JSONArray expand(String fieldName, String jsonString) {
+    public static List<String> expand(String fieldName, String jsonString) {
 
         JSONObject original = new JSONObject(jsonString);
         JSONArray data = original.getJSONArray(fieldName);
@@ -16,14 +19,25 @@ public class JsonTransformer {
         JSONArray out = new JSONArray();
         for (int i = 0; i < data.length(); i++) {
             JSONObject newJson = new JSONObject(reduced);
-            JSONObject d = data.getJSONObject(i);
-            for (Object k : d.keySet()) {
-                newJson.put(k.toString(), d.get(k.toString()));
+            Object object = data.get(i);
+            if ( object instanceof JSONObject ) {
+                JSONObject jsonObject = (JSONObject) object;
+                for (Object k : jsonObject.keySet()) {
+                    newJson.put(k.toString(), jsonObject.get(k.toString()));
+                }
+            } else {
+                newJson.put(fieldName, object);
             }
             int j = i + 1;
             out.put(newJson);
         }
-        return out;
+
+        List<String> jsonObjectsList = new LinkedList();
+        for (int i = 0; i < out.length(); i++) {
+            jsonObjectsList.add(out.get(i).toString());
+        }
+
+        return jsonObjectsList;
     }
 
 }
