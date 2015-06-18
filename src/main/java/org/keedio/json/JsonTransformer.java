3d42ebffd911f6478aba0a1c685aea8ed09e9c3e
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-06-18 14:08:45 +0200

    Move to gson library

--- src/main/java/org/keedio/json/JsonTransformer.java
+++ src/main/java/org/keedio/json/JsonTransformer.java
@@ -1,39 +1,43 @@
 package org.keedio.json;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
 
 public class JsonTransformer {
 
     public static List<String> expand(String fieldName, String jsonString) {
+        Gson gson = new Gson();
 
-        JSONObject original = new JSONObject(jsonString);
-        JSONArray data = original.getJSONArray(fieldName);
+        JsonObject original = gson.fromJson(jsonString, JsonObject.class);
+        JsonArray data = original.getAsJsonArray(fieldName);
 
         original.remove(fieldName);
         String reduced = original.toString();
 
-        JSONArray out = new JSONArray();
-        for (int i = 0; i < data.length(); i++) {
-            JSONObject newJson = new JSONObject(reduced);
+        JsonArray out = new JsonArray();
+        for (int i = 0; i < data.size(); i++) {
+            JsonObject newJson = gson.fromJson(reduced, JsonObject.class);
             Object object = data.get(i);
-            if ( object instanceof JSONObject ) {
-                JSONObject jsonObject = (JSONObject) object;
-                for (Object k : jsonObject.keySet()) {
-                    newJson.put(k.toString(), jsonObject.get(k.toString()));
+            if ( object instanceof JsonObject ) {
+                JsonObject jsonObject = (JsonObject) object;
+                for (Entry<String, JsonElement> k : jsonObject.entrySet()) {
+                    newJson.add(k.getKey(), k.getValue());
                 }
             } else {
-                newJson.put(fieldName, object);
+                newJson.add(fieldName, (JsonElement) object);
             }
             int j = i + 1;
-            out.put(newJson);
+            out.add(newJson);
         }
 
         List<String> jsonObjectsList = new LinkedList();
-        for (int i = 0; i < out.length(); i++) {
+        for (int i = 0; i < out.size(); i++) {
             jsonObjectsList.add(out.get(i).toString());
         }
 
